f0afe4aeb148b4e1d3216a6533b9eb3d514b83f0
ashmander <allwin123456@hotmail.es>
2019-07-02 17:16:37 -0500

    Boton de menu

--- src/components/BarNavigation/index.js
+++ src/components/BarNavigation/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles.css'
+import menu from './../../img/menu.png'
 
 
 
@@ -14,26 +15,25 @@ import './styles.css'
     //addEventListener, removeEventListener
 
     //click, scroll, mouseenter
-    
+
 
 const BarNavigation=({classBar, mainLogo, fixed}) =>{
 
 
         return (
 
-            <div className= {`barNavigation ${classBar} ${(fixed==='no-fixed')?'':'fixed'}`}>    
-                <div className='voidBox'></div>            
-                <a className='icon' href='./index.html'>
-                <img  src={mainLogo} alt=''></img>
-                </a>
-                <nav className='menu'>
+            <div className= {`barNavigation ${classBar} ${(fixed==='no-fixed')?'':'fixed'}`}>
+                <div className='voidBox'></div>
+                  <a className='icon' href='./index.html'>
+                    <img  src={mainLogo} alt=''></img>
+                  </a>
+                  <nav className='menu'>
+                    <img  src={menu} alt='' className="menuBar"></img>
                     <a className='uniform' href="tutorias">Tutorias</a>
                     <a className='uniform' href="trabajar">Sé tutor</a>
                     <a className='uniform' href="contenido">Contenido</a>
                     <a className='special' href="conocenos">Conócenos</a>
-
-                </nav>
-
+                  </nav>
                 </div>
         );
     }
