a112add41ae905a4ebb00d99ddedd9e4d4310069
ashmander <allwin123456@hotmail.es>
2019-07-01 21:43:03 -0500

    Completo botones del registro de estudiante

--- src/pages/RegistroCliente/index.js
+++ src/pages/RegistroCliente/index.js
@@ -7,10 +7,22 @@ import user from './../../img/usuario.png'
 
 import mainLogo from './../../img/mainLogo.png'
 import backgroundStudent from './../../img/registro/backgroundStudent.jpg'
-
+import moreInfo from './../../img/moreInfo.png'
 import './styles.css'
 
 class RegistroE extends Component {
+    moreInfo() {
+      var elementsHiden = document.getElementsByClassName('form');
+      if(elementsHiden[0].style.display === "none") {
+        for (let i = 0; i < elementsHiden.length; i++) {
+          elementsHiden[i].style.display = "block";
+        }
+      } else {
+        for (let i = 0; i < elementsHiden.length; i++) {
+          elementsHiden[i].style.display = "none";
+        }
+      }
+    }
     render() {
         return (
             <div className='divRegister'>
@@ -24,8 +36,8 @@ class RegistroE extends Component {
                         con Red tutores consiguen excelentes resultados  en <br></br>
                         sus exámenes. Anímate y compruebalo.</p>
                         <img src={backgroundStudent} alt=''></img>
-                     </div>   
-                    <form className='formRegister'> 
+                     </div>
+                    <form className='formRegister'>
                     <div className='formIcon'>
                     <img src={user} alt=' '></img>
                     </div>
@@ -33,6 +45,33 @@ class RegistroE extends Component {
                     <input type="text" required placeholder='Nombre(s) y Apellidos'></input>
                     <input type="email" required placeholder='Ingresa tu correo' ></input>
                     <input type='password' required placeholder='Contraseña'></input>
+                    <img className="masInfo" onClick={this.moreInfo} src={moreInfo} alt=' '></img>
+                    <input type='text' required placeholder='Teléfono' className="form"></input>
+                    <input type='text' required placeholder='Codigo Universitario' className="form"></input>
+                    <select className="form" name="pais">
+                      <option value="default">País</option>
+                      <option value="1">Colombia</option>
+                      <option value="2">Venezuela</option>
+                      <option value="3">Chile</option>
+                    </select>
+                    <select className="form" name="departamento">
+                      <option value="default">Departamento</option>
+                      <option value="1">Valle del Cauca</option>
+                      <option value="2">Nariño</option>
+                      <option value="3">Huila</option>
+                    </select>
+                    <select className="form" name="ciudad">
+                      <option value="default">Ciudad</option>
+                      <option value="1">Cali</option>
+                      <option value="2">Palmira</option>
+                      <option value="3">Restrepo</option>
+                    </select>
+                    <select className="form" name="universidad">
+                      <option value="default">Universidad</option>
+                      <option value="1">Universidad Icesi</option>
+                      <option value="2">Universidad Javeriana</option>
+                      <option value="3">Universidad del Valle</option>
+                    </select>
                     <input type='submit' value='Registrarme' formMethod='POST'></input>
                     <br></br>
                     </form>
