e1352992027e568ae8d82156be7991032d845664
julianfelipe27 <juligury@gmail.com>
2019-07-01 22:06:03 -0500

    Mejora de estilos panelRegistro

--- src/components/TutorialFile/index.js
+++ src/components/TutorialFile/index.js
@@ -1,19 +1,39 @@
-import React, { Component } from 'react';
-
+import React from 'react';
+import './styles.css'
 //El componente calendario va a pintar en las fechas que corresponda
 //las tutorias para un usuario con id ?, puede implementarse con un findByDate()
 //y cuando se hace click en un dia se manda la fecha a dicha funcion, por conveniencia
 //esta deberia retornar un JSON del cual se obtendrán las monitorias que tiene 
 //el usuario para dicha fecha con su información completa.
 
-class TutorialFile extends Component {
-    render() {
+const TutorialFile=({date,hourS,hourE, subject, topics, people, price, status })=>{
         return (
-            <div>
-                
+            <div className='tutorialFile'>
+                <div className='separator'>
+                <p className='subtitle'>Fecha: </p> <p>{date}</p>
+                </div>
+                <div className='separator'>
+                <p className='subtitle'>Hora: </p> <p>{hourS}-{hourE}</p>
+                </div>
+                <div className='separator'>
+                <p className='subtitle'>Materia: </p> <p>{subject}</p>
+                </div>
+                <div className='separator'>
+                <p className='subtitle'>Temas: </p> <p>{topics}</p>
+                </div>
+                <div className='separator'>
+                <p className='subtitle'>Personas: </p> <p>{people}</p>
+                </div>
+                <div className='separator'>
+                <p className='subtitle'>Precio: </p> <p>{price}</p>
+                </div>
+                <div className='separator'>
+                <p className='subtitle'>Estado: </p> <p>{status}</p>
+                </div>
             </div>
+
         );
-    }
+        
 }
 
 export default TutorialFile;
