fef995771109dcdc2030b9c73fdf8e2bcd4cb59f
ashmander <allwin123456@hotmail.es>
2019-07-05 20:15:49 -0500

    Barnavigation responsive

--- src/components/BarNavigation/index.js
+++ src/components/BarNavigation/index.js
@@ -16,37 +16,134 @@ import menu from './../../img/menu.png'
 
     //click, scroll, mouseenter
 
+class BarNavigation extends React.Component {
 
- const BarNavigation=({classBar, mainLogo, fixed}) =>{
-   function deployMenu() {
-     var special = document.getElementsByClassName('options');
-     if(special[0].style.display === "none") {
-       for (let i = 0; i < special.length; i++) {
-         special[i].style.display = "block";
-       }
-     } else {
-       for (let i = 0; i < special.length; i++) {
-         special[i].style.display = "none";
-       }
-     }
-   }
-        return (
+  constructor() {
+    super();
+    this.state = {
+      height: window.innerHeight,
+      width: window.innerWidth
+    };
+    this.updateDimensions=this.updateDimensions.bind(this);
+  }
 
-            <div className= {`barNavigation ${classBar} ${(fixed==='no-fixed')?'':'fixed'}`}>
-                <div className='voidBox'></div>
-                  <a className='icon' href='./index.html'>
-                    <img  src={mainLogo} alt=''></img>
-                  </a>
-                  <nav className='menu'>
-                    <img  src={menu} alt='' className="menuBar" onClick={deployMenu}></img>
-                    <a className='options uniform' href="tutorias">Tutorias</a>
-                    <a className='options uniform' href="trabajar">Sé tutor</a>
-                    <a className='options uniform' href="contenido">Contenido</a>
-                    <a className='options special' href="conocenos">Conócenos</a>
-                  </nav>
-                </div>
-        );
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  updateDimensions() {
+    var special = document.getElementsByClassName('options');
+    this.setState({
+      height: window.innerHeight,
+      width: window.innerWidth
+    });
+    var l = this.state.width;
+    if (l > 600) {
+      if(special[0].style.display === "none") {
+        for (let i = 0; i < special.length; i++) {
+          special[i].style.display = "inline-block";
+        }
+      }
     }
+    if (l < 600) {
+      if(special[0].style.display !== "none") {
+        for (let i = 0; i < special.length; i++) {
+          special[i].style.display = "none";
+        }
+      }
+    }
+  }
+
+  deployMenu() {
+    var special = document.getElementsByClassName('options');
+    if(special[0].style.display === "none") {
+      for (let i = 0; i < special.length; i++) {
+        special[i].style.display = "inline-block";
+      }
+    } else {
+      for (let i = 0; i < special.length; i++) {
+        special[i].style.display = "none";
+      }
+    }
+  }
+
+  resize() {
+    var special = document.getElementsByClassName('options');
+    if (document.width() > 600) {
+      if(special[0].style.display === "none") {
+        for (let i = 0; i < special.length; i++) {
+          special[i].style.display = "inline-block";
+        }
+      }
+    }
+  }
+
+  render() {
+    return (
+
+        <div className= {`barNavigation ${this.props.classBar} ${(this.props.fixed==='no-fixed')?'':'fixed'}`}>
+            <div className='voidBox'></div>
+              <a className='icon' href='./index.html'>
+                <img  src={this.props.mainLogo} alt=''></img>
+              </a>
+              <nav className='menu'>
+                <img  src={menu} alt='' className="menuBar" onClick={this.deployMenu}></img>
+                <a className='options uniform' href="tutorias">Tutorias</a>
+                <a className='options uniform' href="trabajar">Sé tutor</a>
+                <a className='options uniform' href="contenido">Contenido</a>
+                <a className='options special' href="conocenos">Conócenos</a>
+              </nav>
+            </div>
+    );
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+}
+
+ // const BarNavigation=({classBar, mainLogo, fixed}) =>{
+ //
+ //   function deployMenu() {
+ //     var special = document.getElementsByClassName('options');
+ //     if(special[0].style.display === "none") {
+ //       for (let i = 0; i < special.length; i++) {
+ //         special[i].style.display = "inline-block";
+ //       }
+ //     } else {
+ //       for (let i = 0; i < special.length; i++) {
+ //         special[i].style.display = "none";
+ //       }
+ //     }
+ //   }
+ //
+ //   function resize() {
+ //     var special = document.getElementsByClassName('options');
+ //     if (document.width() > 600) {
+ //       if(special[0].style.display === "none") {
+ //         for (let i = 0; i < special.length; i++) {
+ //           special[i].style.display = "inline-block";
+ //         }
+ //       }
+ //     }
+ //   }
+ //        return (
+ //
+ //            <div className= {`barNavigation ${classBar} ${(fixed==='no-fixed')?'':'fixed'}`}>
+ //                <div className='voidBox'></div>
+ //                  <a className='icon' href='./index.html'>
+ //                    <img  src={mainLogo} alt=''></img>
+ //                  </a>
+ //                  <nav className='menu'>
+ //                    <img  src={menu} alt='' className="menuBar" onClick={deployMenu}></img>
+ //                    <a className='options uniform' href="tutorias">Tutorias</a>
+ //                    <a className='options uniform' href="trabajar">Sé tutor</a>
+ //                    <a className='options uniform' href="contenido">Contenido</a>
+ //                    <a className='options special' href="conocenos">Conócenos</a>
+ //                  </nav>
+ //                </div>
+ //        );
+ //    }
 
 
 export default BarNavigation;
