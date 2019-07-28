c6f55d108cee710480005bb81a71f3a3a3612320
ashmander <allwin123456@hotmail.es>
2019-07-28 16:52:23 -0500

    Ajustes de la pantalla informacion personal

--- src/pages/ModificarInformacion/components/update-info-client-layout.js
+++ src/pages/ModificarInformacion/components/update-info-client-layout.js
@@ -13,25 +13,25 @@ const UpdateInfoClientLayout =  (props) => (
     <div className="bigContainer">
       <div className="container">
         <h3>Perfil</h3>
-        <div className="info">
+        <div className="infor">
           <label className="left">Foto</label>
           <label className="center">Personaliza tu perfil con tu mejor foto</label>
           <img className="flecha" src={flecha} alt="" />
         </div>
         <hr/>
-        <div className="info">
+        <div className="infor">
           <label className="left">Nombre</label>
           <label className="center">{props.name}</label>
           <img className="flecha" src={flecha} alt="" />
         </div>
         <hr/>
-        <div className="info">
+        <div className="infor">
           <label className="left">Cumpleaños</label>
           <label className="center">coming soon</label>
           <img className="flecha" src={flecha} alt="" />
         </div>
         <hr/>
-        <div className="info">
+        <div className="infor">
           <label className="left">Contraseña</label>
           <label className="center">{props.password}</label>
           <img className="flecha" src={flecha} alt="" />
@@ -39,13 +39,13 @@ const UpdateInfoClientLayout =  (props) => (
       </div>
       <div className="container">
         <h3>Información de contacto</h3>
-        <div className="info">
+        <div className="infor">
           <label className="left">Email</label>
           <label className="center">{props.email}</label>
           <img className="flecha" src={flecha} alt="" />
         </div>
         <hr/>
-        <div className="info">
+        <div className="infor">
           <label className="left">Celular</label>
           <label className="center">{props.phoneNumber}</label>
           <img className="flecha" src={flecha} alt="" />
@@ -53,19 +53,19 @@ const UpdateInfoClientLayout =  (props) => (
       </div>
       <div className="container">
         <h3>Información académica</h3>
-        <div className="info">
+        <div className="infor">
           <label className="left">Universidad</label>
           <label className="center">{props.university}</label>
           <img className="flecha" src={flecha} alt="" />
         </div>
         <hr/>
-        <div className="info">
+        <div className="infor">
           <label className="left">Carrera</label>
           <label className="center">coming soon</label>
           <img className="flecha" src={flecha} alt="" />
         </div>
         <hr/>
-        <div className="info">
+        <div className="infor">
           <label className="left">Código universitario</label>
           <label className="center">{props.universityCode}</label>
           <img className="flecha" src={flecha} alt="" />
@@ -73,19 +73,19 @@ const UpdateInfoClientLayout =  (props) => (
       </div>
       <div className="container">
         <h3>Residencia</h3>
-        <div className="info">
+        <div className="infor">
           <label className="left">País</label>
           <label className="center">{props.country}</label>
           <img className="flecha" src={flecha} alt="" />
         </div>
         <hr/>
-        <div className="info">
+        <div className="infor">
           <label className="left">Departamento</label>
           <label className="center">{props.department}</label>
           <img className="flecha" src={flecha} alt="" />
         </div>
         <hr/>
-        <div className="info">
+        <div className="infor">
           <label className="left">Ciudad</label>
           <label className="center">{props.city}</label>
           <img className="flecha" src={flecha} alt="" />
