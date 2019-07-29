b2efb32301c81973012b7d89af9c5dbf6d774b0a
ashmander <allwin123456@hotmail.es>
2019-07-28 21:28:57 -0500

    Pantalla de informacion personal lista

--- src/pages/ModificarInformacion/components/update-info-client-layout.js
+++ src/pages/ModificarInformacion/components/update-info-client-layout.js
@@ -16,79 +16,102 @@ const UpdateInfoClientLayout =  (props) => (
         <div className="infor">
           <label className="left">Foto</label>
           <label className="center">Personaliza tu perfil con tu mejor foto</label>
-          <img className="flecha" src={flecha} alt="" />
+          <img className="flecha" src={flecha} alt="" onClick={props.handleClickEdit} ref={props.setRefArrow}/>
+          <input className="guardar" type="submit" value="Guardar" ref={props.setRefGuardar} onClick={props.handleGuardarClick}/>
         </div>
         <hr/>
         <div className="infor">
           <label className="left">Nombre</label>
-          <label className="center">{props.name}</label>
-          <img className="flecha" src={flecha} alt="" />
-        </div>
-        <hr/>
-        <div className="infor">
-          <label className="left">Cumpleaños</label>
-          <label className="center">coming soon</label>
-          <img className="flecha" src={flecha} alt="" />
+          <label className="center" ref={props.setRefNameLabel}>{props.name}</label>
+          <input className="center editar" type="text" value={props.name} ref={props.setRefName} onChange={props.handleNameChange}/>
+          <img className="flecha" src={flecha} alt="" onClick={props.handleClickEditName} ref={props.setRefArrowName}/>
+          <input className="guardar" type="submit" value="Guardar" ref={props.setRefGuardarName} onClick={props.handleGuardarClickName}/>
         </div>
         <hr/>
         <div className="infor">
           <label className="left">Contraseña</label>
-          <label className="center">{props.password}</label>
-          <img className="flecha" src={flecha} alt="" />
+          <label className="center" ref={props.setRefPasswordLabel}>{props.password}</label>
+          <input className="center editar" type="password" value={props.password} onChange={props.handlePasswordChange} ref={props.setRefPassword}/>
+          <img className="flecha" src={flecha} alt="" onClick={props.handleClickEditPassword} ref={props.setRefArrowPassword}/>
+          <input className="guardar" type="submit" value="Guardar" ref={props.setRefGuardarPassword} onClick={props.handleGuardarClickPassword}/>
         </div>
       </div>
       <div className="container">
         <h3>Información de contacto</h3>
         <div className="infor">
           <label className="left">Email</label>
-          <label className="center">{props.email}</label>
-          <img className="flecha" src={flecha} alt="" />
+          <label className="center" ref={props.setRefEmailLabel}>{props.email}</label>
+          <input className="center editar" type="email" value={props.email} onChange={props.handleEmailChange} ref={props.setRefEmail}/>
+          <img className="flecha" src={flecha} alt="" onClick={props.handleClickEditEmail} ref={props.setRefArrowEmail}/>
+          <input className="guardar" type="submit" value="Guardar" ref={props.setRefGuardarEmail} onClick={props.handleGuardarClickEmail}/>
         </div>
         <hr/>
         <div className="infor">
           <label className="left">Celular</label>
-          <label className="center">{props.phoneNumber}</label>
-          <img className="flecha" src={flecha} alt="" />
+          <label className="center" ref={props.setRefPhoneNumberLabel}>{props.phoneNumber}</label>
+          <input className="center editar" type="text" value={props.phoneNumber} onChange={props.handlePhoneNumberChange} ref={props.setRefPhoneNumber} maxLength={10} minLength={10}/>
+          <img className="flecha" src={flecha} alt="" onClick={props.handleClickEditPhoneNumber} ref={props.setRefArrowPhoneNumber}/>
+          <input className="guardar" type="submit" value="Guardar" ref={props.setRefGuardarPhoneNumber} onClick={props.handleGuardarClickPhoneNumber}/>
         </div>
       </div>
       <div className="container">
         <h3>Información académica</h3>
         <div className="infor">
           <label className="left">Universidad</label>
-          <label className="center">{props.university}</label>
-          <img className="flecha" src={flecha} alt="" />
-        </div>
-        <hr/>
-        <div className="infor">
-          <label className="left">Carrera</label>
-          <label className="center">coming soon</label>
-          <img className="flecha" src={flecha} alt="" />
+          <label className="center" ref={props.setRefUniversityLabel}>{props.university}</label>
+          <select className="center editar" value={props.university} onChange={props.handleUniversityChange} ref={props.setRefUniversity}>
+            <option value="icesi">Universidad Icesi</option>
+            <option value="javeriana">Universidad Javeriana</option>
+            <option value="valle">Universidad del Valle</option>
+          </select>
+          <img className="flecha" src={flecha} alt="" onClick={props.handleClickEditUniversity} ref={props.setRefArrowUniversity}/>
+          <input className="guardar" type="submit" value="Guardar" ref={props.setRefGuardarUniversity} onClick={props.handleGuardarClickUniversity}/>
         </div>
         <hr/>
         <div className="infor">
           <label className="left">Código universitario</label>
-          <label className="center">{props.universityCode}</label>
-          <img className="flecha" src={flecha} alt="" />
+          <label className="center" ref={props.setRefUniversityCodeLabel}>{props.universityCode}</label>
+          <input className="center editar" type="text" value={props.universityCode} onChange={props.handleUniversityCodeChange} ref={props.setRefUniversityCode}/>
+          <img className="flecha" src={flecha} alt="" onClick={props.handleClickEditUniversityCode} ref={props.setRefArrowUniversityCode}/>
+          <input className="guardar" type="submit" value="Guardar" ref={props.setRefGuardarUniversityCode} onClick={props.handleGuardarClickUniversityCode}/>
         </div>
       </div>
       <div className="container">
         <h3>Residencia</h3>
         <div className="infor">
           <label className="left">País</label>
-          <label className="center">{props.country}</label>
-          <img className="flecha" src={flecha} alt="" />
+          <label className="center" ref={props.setRefCountryLabel}>{props.country}</label>
+          <select className="center editar" value={props.country} onChange={props.handleCountryChange} ref={props.setRefCountry}>
+            <option value="colombia">Colombia</option>
+            <option value="venezuela">Venezuela</option>
+            <option value="chile">Chile</option>
+          </select>
+          <img className="flecha" src={flecha} alt="" onClick={props.handleClickEditCountry} ref={props.setRefArrowCountry}/>
+          <input className="guardar" type="submit" value="Guardar" ref={props.setRefGuardarCountry} onClick={props.handleGuardarClickCountry}/>
         </div>
         <hr/>
         <div className="infor">
           <label className="left">Departamento</label>
-          <label className="center">{props.department}</label>
-          <img className="flecha" src={flecha} alt="" />
+          <label className="center" ref={props.setRefDepartmentLabel}>{props.department}</label>
+          <select className="center editar" value={props.department} onChange={props.handleDepartmentChange} ref={props.setRefDepartment}>
+            <option value="Valle del Cauca">Valle del Cauca</option>
+            <option value="Nariño">Nariño</option>
+            <option value="Huila">Huila</option>
+          </select>
+          <img className="flecha" src={flecha} alt="" onClick={props.handleClickEditDepartment} ref={props.setRefArrowDepartment}/>
+          <input className="guardar" type="submit" value="Guardar" ref={props.setRefGuardarDepartment} onClick={props.handleGuardarClickDepartment}/>
         </div>
         <hr/>
         <div className="infor">
           <label className="left">Ciudad</label>
-          <label className="center">{props.city}</label>
-          <img className="flecha" src={flecha} alt="" />
+          <label className="center" ref={props.setRefCityLabel}>{props.city}</label>
+          <select className="center editar" value={props.city} onChange={props.handleCityChange} ref={props.setRefCity}>
+            <option value="Cali">Cali</option>
+            <option value="Palmira">Palmira</option>
+            <option value="Restrepo">Restrepo</option>
+          </select>
+          <img className="flecha" src={flecha} alt="" onClick={props.handleClickEditCity} ref={props.setRefArrowCity}/>
+          <input className="guardar" type="submit" value="Guardar" ref={props.setRefGuardarCity} onClick={props.handleGuardarClickCity}/>
         </div>
       </div>
     </div>
--- src/pages/ModificarInformacion/containers/update-info-client.js
+++ src/pages/ModificarInformacion/containers/update-info-client.js
@@ -86,11 +86,247 @@ class UpdateInfoClient extends Component {
     });
   }
 
+
+  handleClickEditName = event => {
+    this.arrowName.style.display = 'none';
+    this.guardarName.style.display = 'inline-block';
+    this.name.style.display = "inline-block";
+    this.nameLabel.style.display = "none";
+  }
+  handleGuardarClickName = event => {
+    this.arrowName.style.display = 'inline-block';
+    this.guardarName.style.display = 'none';
+    this.name.style.display = "none";
+    this.nameLabel.style.display = "inline-block";
+  }
+  setRefArrowName = element =>  {
+    this.arrowName = element;
+  }
+  setRefGuardarName = element => {
+    this.guardarName = element;
+  }
+  setRefName = element => {
+    this.name = element;
+  }
+  setRefNameLabel = element => {
+    this.nameLabel = element;
+  }
+
+
+  handleClickEditPassword = event => {
+    this.arrowPassword.style.display = 'none';
+    this.guardarPassword.style.display = 'inline-block';
+    this.password.style.display = "inline-block";
+    this.passwordLabel.style.display = "none";
+  }
+  handleGuardarClickPassword = event => {
+    this.arrowPassword.style.display = 'inline-block';
+    this.guardarPassword.style.display = 'none';
+    this.password.style.display = "none";
+    this.passwordLabel.style.display = "inline-block";
+  }
+  setRefArrowPassword = element =>  {
+    this.arrowPassword = element;
+  }
+  setRefGuardarPassword = element => {
+    this.guardarPassword = element;
+  }
+  setRefPassword = element => {
+    this.password = element;
+  }
+  setRefPasswordLabel = element => {
+    this.passwordLabel = element;
+  }
+
+
+  handleClickEditEmail = event => {
+    this.arrowEmail.style.display = 'none';
+    this.guardarEmail.style.display = 'inline-block';
+    this.email.style.display = "inline-block";
+    this.emailLabel.style.display = "none";
+  }
+  handleGuardarClickEmail = event => {
+    this.arrowEmail.style.display = 'inline-block';
+    this.guardarEmail.style.display = 'none';
+    this.email.style.display = "none";
+    this.emailLabel.style.display = "inline-block";
+  }
+  setRefArrowEmail = element =>  {
+    this.arrowEmail = element;
+  }
+  setRefGuardarEmail = element => {
+    this.guardarEmail = element;
+  }
+  setRefEmail = element => {
+    this.email = element;
+  }
+  setRefEmailLabel = element => {
+    this.emailLabel = element;
+  }
+
+
+  handleClickEditPhoneNumber = event => {
+    this.arrowPhoneNumber.style.display = 'none';
+    this.guardarPhoneNumber.style.display = 'inline-block';
+    this.phoneNumber.style.display = "inline-block";
+    this.phoneNumberLabel.style.display = "none";
+  }
+  handleGuardarClickPhoneNumber = event => {
+    this.arrowPhoneNumber.style.display = 'inline-block';
+    this.guardarPhoneNumber.style.display = 'none';
+    this.phoneNumber.style.display = "none";
+    this.phoneNumberLabel.style.display = "inline-block";
+  }
+  setRefArrowPhoneNumber = element =>  {
+    this.arrowPhoneNumber = element;
+  }
+  setRefGuardarPhoneNumber = element => {
+    this.guardarPhoneNumber = element;
+  }
+  setRefPhoneNumber = element => {
+    this.phoneNumber = element;
+  }
+  setRefPhoneNumberLabel = element => {
+    this.phoneNumberLabel = element;
+  }
+
+
+  handleClickEditUniversity = event => {
+    this.arrowUniversity.style.display = 'none';
+    this.guardarUniversity.style.display = 'inline-block';
+    this.university.style.display = "inline-block";
+    this.universityLabel.style.display = "none";
+  }
+  handleGuardarClickUniversity = event => {
+    this.arrowUniversity.style.display = 'inline-block';
+    this.guardarUniversity.style.display = 'none';
+    this.university.style.display = "none";
+    this.universityLabel.style.display = "inline-block";
+  }
+  setRefArrowUniversity = element =>  {
+    this.arrowUniversity = element;
+  }
+  setRefGuardarUniversity = element => {
+    this.guardarUniversity = element;
+  }
+  setRefUniversity = element => {
+    this.university = element;
+  }
+  setRefUniversityLabel = element => {
+    this.universityLabel = element;
+  }
+
+
+  handleClickEditUniversityCode = event => {
+    this.arrowUniversityCode.style.display = 'none';
+    this.guardarUniversityCode.style.display = 'inline-block';
+    this.universityCode.style.display = "inline-block";
+    this.universityCodeLabel.style.display = "none";
+  }
+  handleGuardarClickUniversityCode = event => {
+    this.arrowUniversityCode.style.display = 'inline-block';
+    this.guardarUniversityCode.style.display = 'none';
+    this.universityCode.style.display = "none";
+    this.universityCodeLabel.style.display = "inline-block";
+  }
+  setRefArrowUniversityCode = element =>  {
+    this.arrowUniversityCode = element;
+  }
+  setRefGuardarUniversityCode = element => {
+    this.guardarUniversityCode = element;
+  }
+  setRefUniversityCode = element => {
+    this.universityCode = element;
+  }
+  setRefUniversityCodeLabel = element => {
+    this.universityCodeLabel = element;
+  }
+
+
+  handleClickEditCountry = event => {
+    this.arrowCountry.style.display = 'none';
+    this.guardarCountry.style.display = 'inline-block';
+    this.country.style.display = "inline-block";
+    this.countryLabel.style.display = "none";
+  }
+  handleGuardarClickCountry = event => {
+    this.arrowCountry.style.display = 'inline-block';
+    this.guardarCountry.style.display = 'none';
+    this.country.style.display = "none";
+    this.countryLabel.style.display = "inline-block";
+  }
+  setRefArrowCountry = element =>  {
+    this.arrowCountry = element;
+  }
+  setRefGuardarCountry = element => {
+    this.guardarCountry = element;
+  }
+  setRefCountry = element => {
+    this.country = element;
+  }
+  setRefCountryLabel = element => {
+    this.countryLabel = element;
+  }
+
+
+  handleClickEditDepartment = event => {
+    this.arrowDepartment.style.display = 'none';
+    this.guardarDepartment.style.display = 'inline-block';
+    this.department.style.display = "inline-block";
+    this.departmentLabel.style.display = "none";
+  }
+  handleGuardarClickDepartment = event => {
+    this.arrowDepartment.style.display = 'inline-block';
+    this.guardarDepartment.style.display = 'none';
+    this.department.style.display = "none";
+    this.departmentLabel.style.display = "inline-block";
+  }
+  setRefArrowDepartment = element =>  {
+    this.arrowDepartment = element;
+  }
+  setRefGuardarDepartment = element => {
+    this.guardarDepartment = element;
+  }
+  setRefDepartment = element => {
+    this.department = element;
+  }
+  setRefDepartmentLabel = element => {
+    this.departmentLabel = element;
+  }
+
+
+  handleClickEditCity = event => {
+    this.arrowCity.style.display = 'none';
+    this.guardarCity.style.display = 'inline-block';
+    this.city.style.display = "inline-block";
+    this.cityLabel.style.display = "none";
+  }
+  handleGuardarClickCity = event => {
+    this.arrowCity.style.display = 'inline-block';
+    this.guardarCity.style.display = 'none';
+    this.city.style.display = "none";
+    this.cityLabel.style.display = "inline-block";
+  }
+  setRefArrowCity = element =>  {
+    this.arrowCity = element;
+  }
+  setRefGuardarCity = element => {
+    this.guardarCity = element;
+  }
+  setRefCity = element => {
+    this.city = element;
+  }
+  setRefCityLabel = element => {
+    this.cityLabel = element;
+  }
+
+
   render() {
     return (
       <div>
         <section>
           <UpdateInfoClientLayout
+
             name = {this.state.name}
             lastName = {this.state.lastName}
             phoneNumber = {this.state.phoneNumber}
@@ -101,6 +337,7 @@ class UpdateInfoClient extends Component {
             department = {this.state.department}
             city = {this.state.city}
             email = {this.state.email}
+
             handleUniversityChange = {this.handleUniversityChange}
             handleNameChange = {this.handleNameChange}
             handleLastNameChange = {this.handleLastNameChange}
@@ -111,6 +348,69 @@ class UpdateInfoClient extends Component {
             handleCityChange = {this.handleCityChange}
             handlePhoneNumberChange = {this.handlePhoneNumberChange}
             handleEmailChange = {this.handleEmailChange}
+
+            setRefArrowName = {this.setRefArrowName}
+            handleClickEditName = {this.handleClickEditName}
+            setRefGuardarName = {this.setRefGuardarName}
+            setRefName =  {this.setRefName}
+            setRefNameLabel = {this.setRefNameLabel}
+            handleGuardarClickName = {this.handleGuardarClickName}
+
+            setRefArrowPassword = {this.setRefArrowPassword}
+            handleClickEditPassword = {this.handleClickEditPassword}
+            setRefGuardarPassword = {this.setRefGuardarPassword}
+            setRefPassword =  {this.setRefPassword}
+            setRefPasswordLabel = {this.setRefPasswordLabel}
+            handleGuardarClickPassword = {this.handleGuardarClickPassword}
+
+            setRefArrowEmail = {this.setRefArrowEmail}
+            handleClickEditEmail = {this.handleClickEditEmail}
+            setRefGuardarEmail = {this.setRefGuardarEmail}
+            setRefEmail =  {this.setRefEmail}
+            setRefEmailLabel = {this.setRefEmailLabel}
+            handleGuardarClickEmail = {this.handleGuardarClickEmail}
+
+            setRefArrowPhoneNumber = {this.setRefArrowPhoneNumber}
+            handleClickEditPhoneNumber = {this.handleClickEditPhoneNumber}
+            setRefGuardarPhoneNumber = {this.setRefGuardarPhoneNumber}
+            setRefPhoneNumber =  {this.setRefPhoneNumber}
+            setRefPhoneNumberLabel = {this.setRefPhoneNumberLabel}
+            handleGuardarClickPhoneNumber = {this.handleGuardarClickPhoneNumber}
+
+            setRefArrowUniversity = {this.setRefArrowUniversity}
+            handleClickEditUniversity = {this.handleClickEditUniversity}
+            setRefGuardarUniversity = {this.setRefGuardarUniversity}
+            setRefUniversity =  {this.setRefUniversity}
+            setRefUniversityLabel = {this.setRefUniversityLabel}
+            handleGuardarClickUniversity = {this.handleGuardarClickUniversity}
+
+            setRefArrowUniversityCode = {this.setRefArrowUniversityCode}
+            handleClickEditUniversityCode = {this.handleClickEditUniversityCode}
+            setRefGuardarUniversityCode = {this.setRefGuardarUniversityCode}
+            setRefUniversityCode =  {this.setRefUniversityCode}
+            setRefUniversityCodeLabel = {this.setRefUniversityCodeLabel}
+            handleGuardarClickUniversityCode = {this.handleGuardarClickUniversityCode}
+
+            setRefArrowCountry = {this.setRefArrowCountry}
+            handleClickEditCountry = {this.handleClickEditCountry}
+            setRefGuardarCountry = {this.setRefGuardarCountry}
+            setRefCountry =  {this.setRefCountry}
+            setRefCountryLabel = {this.setRefCountryLabel}
+            handleGuardarClickCountry = {this.handleGuardarClickCountry}
+
+            setRefArrowDepartment = {this.setRefArrowDepartment}
+            handleClickEditDepartment = {this.handleClickEditDepartment}
+            setRefGuardarDepartment = {this.setRefGuardarDepartment}
+            setRefDepartment =  {this.setRefDepartment}
+            setRefDepartmentLabel = {this.setRefDepartmentLabel}
+            handleGuardarClickDepartment = {this.handleGuardarClickDepartment}
+
+            setRefArrowCity = {this.setRefArrowCity}
+            handleClickEditCity = {this.handleClickEditCity}
+            setRefGuardarCity = {this.setRefGuardarCity}
+            setRefCity =  {this.setRefCity}
+            setRefCityLabel = {this.setRefCityLabel}
+            handleGuardarClickCity = {this.handleGuardarClickCity}
           />
         </section>
       </div>
